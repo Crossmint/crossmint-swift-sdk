d0cdd61d6a3efd7ee47f2765251a764871f9bc12
jcurbelo <robin@paella.dev>
2025-11-14 15:00:56 -0500

    adds collectionLocator and callData props

--- Sources/Checkout/Models/CheckoutLineItems.swift
+++ Sources/Checkout/Models/CheckoutLineItems.swift
@@ -8,21 +8,36 @@
 import Foundation
 
 public struct CheckoutLineItems {
-    public let tokenLocator: String
+    public let tokenLocator: String?
+    public let collectionLocator: String?
     public let executionParameters: [String: Any]?
+    public let callData: [String: Any]?
     
     public init(
-        tokenLocator: String,
-        executionParameters: [String: Any]? = nil
+        tokenLocator: String? = nil,
+        collectionLocator: String? = nil,
+        executionParameters: [String: Any]? = nil,
+        callData: [String: Any]? = nil
     ) {
         self.tokenLocator = tokenLocator
+        self.collectionLocator = collectionLocator
         self.executionParameters = executionParameters
+        self.callData = callData
     }
     
     func toDictionary() -> [String: Any] {
-        var dict: [String: Any] = ["tokenLocator": tokenLocator]
+        var dict: [String: Any] = [:]
+        if let tokenLocator = tokenLocator {
+            dict["tokenLocator"] = tokenLocator
+        }
+        if let collectionLocator = collectionLocator {
+            dict["collectionLocator"] = collectionLocator
+        }
         if let executionParameters = executionParameters {
             dict["executionParameters"] = executionParameters
+        }
+        if let callData = callData {
+            dict["callData"] = callData
         }
         return dict
     }
