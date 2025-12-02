93017dea5045577df6fd94b5a228d331372cb29b
Tomás Martins <tomas.temp@paella.dev>
2025-12-02 16:44:36 -0300

    Removed level check from OSLoggerProvider

--- Sources/Logger/OSLoggerProvider.swift
+++ Sources/Logger/OSLoggerProvider.swift
@@ -18,22 +18,18 @@ public class OSLoggerProvider: LoggerProvider {
     }
     
     func debug(_ message: String, attributes: [String : any Encodable]?) {
-        guard Logger.level == .debug else { return }
         os_log(.debug, log: osLogger, "%{public}@", message)
     }
     
     func error(_ message: String, attributes: [String : any Encodable]?) {
-        guard Logger.level != .fault else { return }
         os_log(.error, log: osLogger, "%{public}@", message)
     }
     
     func info(_ message: String, attributes: [String : any Encodable]?) {
-        guard [.debug, .info].contains(Logger.level) else { return }
         os_log(.info, log: osLogger, "%{public}@", message)
     }
     
     func warn(_ message: String, attributes: [String : any Encodable]?) {
-        guard [.debug, .info, .default].contains(Logger.level) else { return }
         os_log(.default, log: osLogger, "%{public}@", message)
     }
 }
