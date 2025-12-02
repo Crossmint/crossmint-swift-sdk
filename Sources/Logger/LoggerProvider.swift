f0fe916fe930e986a07b67369ad53af21a80d5cb
Tomás Martins <tomas.temp@paella.dev>
2025-12-02 15:32:09 -0300

    Added attributes to LoggerProvider protocol

--- Sources/Logger/LoggerProvider.swift
+++ Sources/Logger/LoggerProvider.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 protocol LoggerProvider: Sendable {
-    func debug(_ message: String)
-    func error(_ message: String)
-    func info(_ message: String)
-    func warn(_ message: String)
+    func debug(_ message: String, attributes: [String: Encodable]?)
+    func error(_ message: String, attributes: [String: Encodable]?)
+    func info(_ message: String, attributes: [String: Encodable]?)
+    func warn(_ message: String, attributes: [String: Encodable]?)
 }
