58850679b9f3ddfd9b4dc7103c087ce66c193acf
brendanw <brendan@basebeta.com>
2025-12-04 14:33:15 -0700

    fix issue where iOS does not correctly follow the spec for how to encode the + symbol in query parameters

--- Examples/SmartWalletsDemo/SmartWalletsDemo/Login/CrossmintAuthManager.swift
+++ Examples/SmartWalletsDemo/SmartWalletsDemo/Login/CrossmintAuthManager.swift
@@ -8,6 +8,6 @@
 import Auth
 import CrossmintClient
 
-let crossmintApiKey = "ck_staging_YOUR_API_KEY"
+let crossmintApiKey = "ck_staging_9tb52Q6BLj8aeLEynQcpyrpGZ5kBbTVjLN1aH5tiyx9D6YWyS76bPm1cPQWnchVNGfwQjZsF92GE5gGUGPQjtpDVDwAaz9PXPw74Udo6WjeRjpiXJ24TDV29DCYDmAoTkbNN865KEDZKHrqUHEBh8dBvcYLes2HCjx5fiCysFS1MUU4mjYUk6taftpSLHMHVvGAbbXSAqCRnuvxda1sUMEqg"
 // swiftlint:disable:next force_try
 let crossmintAuthManager = try! CrossmintAuthManager(apiKey: crossmintApiKey)
--- Sources/CrossmintService/DefaultRequestBuilder.swift
+++ Sources/CrossmintService/DefaultRequestBuilder.swift
@@ -19,6 +19,9 @@ public struct DefaultRequestBuilder: RequestBuilder {
         }
         components.path += endpoint.path
         components.queryItems = endpoint.queryItems
+       
+        components.percentEncodedQuery = components.percentEncodedQuery?
+          .replacingOccurrences(of: "+", with: "%2B")
 
         guard let componentsUrl = components.url else {
             throw .invalidURL
