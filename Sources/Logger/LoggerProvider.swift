86baeac867f1dd222f7654a545ba5a51dd58b7f1
Tomás Martins <tomas.temp@paella.dev>
2025-12-24 12:39:26 -0300

    Updated LoggerProvider protocol for actor compatibility

--- Sources/Logger/LoggerProvider.swift
+++ Sources/Logger/LoggerProvider.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 protocol LoggerProvider: Sendable {
-    func debug(_ message: String, attributes: [String: Encodable]?)
-    func error(_ message: String, attributes: [String: Encodable]?)
-    func info(_ message: String, attributes: [String: Encodable]?)
-    func warn(_ message: String, attributes: [String: Encodable]?)
+    nonisolated func debug(_ message: String, attributes: [String: Encodable]?)
+    nonisolated func error(_ message: String, attributes: [String: Encodable]?)
+    nonisolated func info(_ message: String, attributes: [String: Encodable]?)
+    nonisolated func warn(_ message: String, attributes: [String: Encodable]?)
 }
