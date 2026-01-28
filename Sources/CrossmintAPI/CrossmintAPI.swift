a8d02755ebc7b0fae0eb54272c754f4cd3a2272e
Tomás Martins <tomas.temp@paella.dev>
2026-01-28 19:36:17 -0300

    Initial implementation of OpenAPI spec generated client

---
+++ Sources/CrossmintAPI/CrossmintAPI.swift
@@ -0,0 +1,11 @@
+// CrossmintAPI
+// Generated HTTP client from OpenAPI specification
+//
+// To regenerate the code, run:
+//   make generate-api
+//
+// The generated code is located in the GeneratedSources directory.
+// swiftlint:disable all
+
+@_exported import OpenAPIRuntime
+@_exported import OpenAPIURLSession
