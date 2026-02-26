15cf7db17dc2ec1da2860bd823a5ff7538163efa
Tomás Martins <tomas.temp@paella.dev>
2026-02-26 17:46:47 -0300

    Remove Sources/Payments

--- Sources/Payments/HeadlessCheckout/Order/Models/OrderQuoteStatus.swift
+++
@@ -1,7 +0,0 @@
-public enum OrderQuoteStatus: String, Codable, Sendable {
-    case requiresRecipient = "requires-recipient"
-    case requiresPhysicalAddress = "requires-physical-address"
-    case allLineItemsUnavailable = "all-line-items-unavailable"
-    case valid = "valid"
-    case expired = "expired"
-}
