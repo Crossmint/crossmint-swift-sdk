6873e6a159f61b1331053bbf2b0292344ce7e804
Tomás Martins <tomas.temp@paella.dev>
2026-03-04 17:40:12 -0300

    Fix delegatedSigners placement inside config in createWallet request

--- Sources/Wallet/Data/CreateWallet/CreateWalletParams.swift
+++ Sources/Wallet/Data/CreateWallet/CreateWalletParams.swift
@@ -1,36 +1,30 @@
 import CrossmintCommonTypes
 import Passkeys
 
-public struct DelegatedSignerPublicKey: Encodable {
-    public let x: String
-    public let y: String
-}
-
-public struct DelegatedSignerData: Encodable {
-    public let type: String = "device"
-    public let publicKey: DelegatedSignerPublicKey
-}
-
 public struct DelegatedSignerEntry: Encodable {
-    public let signer: DelegatedSignerData
+    public let signer: String  // e.g. "device:<base64_uncompressed_pubkey>"
 }
 
 public struct CreateWalletParams: Encodable {
     struct InputConfig: Encodable {
         let adminSigner: any AdminSignerData
+        let delegatedSigners: [DelegatedSignerEntry]?
 
         enum CodingKeys: String, CodingKey {
             case adminSigner
+            case delegatedSigners
         }
 
         public func encode(to encoder: Encoder) throws {
             var container = encoder.container(keyedBy: CodingKeys.self)
             try container.encode(adminSigner, forKey: .adminSigner)
+            if let delegatedSigners {
+                try container.encode(delegatedSigners, forKey: .delegatedSigners)
+            }
         }
     }
 
     let chainType: ChainType
     let type: WalletType
     let config: InputConfig
-    let delegatedSigners: [DelegatedSignerEntry]?
 }
