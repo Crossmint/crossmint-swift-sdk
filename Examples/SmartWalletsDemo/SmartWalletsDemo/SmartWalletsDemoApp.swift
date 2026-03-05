56ed137c00fac8e1a136b049ec511a8bb8521aa5
Tomás Martins <tomas.temp@paella.dev>
2026-03-05 17:34:35 -0300

    Undo log level change for demo app

--- Examples/SmartWalletsDemo/SmartWalletsDemo/SmartWalletsDemoApp.swift
+++ Examples/SmartWalletsDemo/SmartWalletsDemo/SmartWalletsDemoApp.swift
@@ -7,7 +7,7 @@ struct SmartWalletsDemoApp: App {
         WindowGroup {
             SplashScreen()
                 .crossmintNonCustodialSigner(
-                    CrossmintSDK.shared(apiKey: crossmintApiKey, authManager: crossmintAuthManager, logLevel: .debug)
+                    CrossmintSDK.shared(apiKey: crossmintApiKey, authManager: crossmintAuthManager)
                 )
         }
     }
