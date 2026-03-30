bc5b113abacae6953efeb15f1e30e01e07950e8d
Tomás Martins <tomas.temp@paella.dev>
2026-03-30 12:51:22 -0300

    Add signerNotRegistered error case to WalletError

--- Sources/Wallet/Model/Wallet/WalletError.swift
+++ Sources/Wallet/Model/Wallet/WalletError.swift
@@ -15,6 +15,7 @@ public enum WalletError: ServiceError {
     case transactionNotFound
     case invalidChain(chain: Chain)
     case invalidToken(token: CryptoCurrency)
+    case signerNotRegistered(String)
 
     public var errorMessage: String {
         switch self {
@@ -39,6 +40,8 @@ public enum WalletError: ServiceError {
             return "Invalid chain: \(chain.name)"
         case .invalidToken(let token):
             return "Invalid token: \(token.name)"
+        case .signerNotRegistered(let locator):
+            return "Signer \"\(locator)\" is not registered on this wallet. Call addSigner first."
         }
     }
 
