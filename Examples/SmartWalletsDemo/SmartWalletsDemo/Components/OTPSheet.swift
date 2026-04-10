e4c148a3597e6430285b9a6f68d9916fd126265d
Tomás Martins <tomas.temp@paella.dev>
2026-04-10 13:27:31 -0300

    Clean up SmartWalletsDemo app

---
+++ Examples/SmartWalletsDemo/SmartWalletsDemo/Components/OTPSheet.swift
@@ -0,0 +1,23 @@
+//
+//  OTPSheet.swift
+//  SmartWalletsDemo
+//
+
+import CrossmintClient
+import SwiftUI
+
+extension View {
+    func otpSheet() -> some View {
+        modifier(OTPSheetModifier())
+    }
+}
+
+private struct OTPSheetModifier: ViewModifier {
+    @State private var showOTPView = false
+
+    func body(content: Content) -> some View {
+        content
+            .sheet(isPresented: $showOTPView) { OTPValidatorView() }
+            .onReceive(CrossmintSDK.shared.isOTPRequired) { showOTPView = $0 }
+    }
+}
