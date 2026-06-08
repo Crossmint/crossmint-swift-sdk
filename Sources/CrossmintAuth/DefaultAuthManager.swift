25e6f26cb9b3c63963c663733aa6b1360c157f80
tomas-martins-crossmint <tomas.temp@paella.dev>
2026-06-08 11:24:26 -0300

    Treat all JWT refresh failures as nonAuthenticated to break auth error loop

--- Sources/CrossmintAuth/DefaultAuthManager.swift
+++ Sources/CrossmintAuth/DefaultAuthManager.swift
@@ -223,12 +223,8 @@ public actor CrossmintAuthManager: AuthManager {
             _authenticationStatus = authStatus
             return authStatus
         } catch {
-            if case .signInRequired = error {
-                _authenticationStatus = .nonAuthenticated
-            } else {
-                _authenticationStatus = nil
-            }
-            throw error
+            _authenticationStatus = .nonAuthenticated
+            throw AuthError.signInRequired
         }
     }
 }
