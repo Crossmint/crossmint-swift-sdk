dcc00f146088e49ecdc4caebd85e89bb6a414a8a
tomas-martins-crossmint <tomas.temp@paella.dev>
2026-07-14 11:00:20 -0300

    Rely on synthesized Encodable to omit nil body fields

--- Sources/Wallet/Data/CreateWallet/Signers/RegisterSignerRequest.swift
+++ Sources/Wallet/Data/CreateWallet/Signers/RegisterSignerRequest.swift
@@ -6,38 +6,12 @@ struct RegisterSignerBody: Encodable {
     let signer: String
     let chain: String?
     let deployImmediately: Bool?
-
-    private enum CodingKeys: String, CodingKey {
-        case signer
-        case chain
-        case deployImmediately
-    }
-
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(signer, forKey: .signer)
-        try container.encodeIfPresent(chain, forKey: .chain)
-        try container.encodeIfPresent(deployImmediately, forKey: .deployImmediately)
-    }
 }
 
 struct RegisterTypedSignerBody: Encodable {
     let signer: AdminSignerRequestApiModel
     let chain: String?
     let deployImmediately: Bool?
-
-    private enum CodingKeys: String, CodingKey {
-        case signer
-        case chain
-        case deployImmediately
-    }
-
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(signer, forKey: .signer)
-        try container.encodeIfPresent(chain, forKey: .chain)
-        try container.encodeIfPresent(deployImmediately, forKey: .deployImmediately)
-    }
 }
 
 extension Endpoint {
