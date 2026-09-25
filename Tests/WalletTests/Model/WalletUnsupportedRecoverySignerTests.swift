a8f76ec8bfdff9525ed486caca3a9a50f57ae135
tomas-martins-crossmint <tomas.temp@paella.dev>
2026-09-25 10:42:54 -0300

    Match the unsupported signer type without quotes since the raw error dump escapes them

--- Tests/WalletTests/Model/WalletUnsupportedRecoverySignerTests.swift
+++ Tests/WalletTests/Model/WalletUnsupportedRecoverySignerTests.swift
@@ -52,7 +52,7 @@ struct WalletUnsupportedRecoverySignerTests {
             try DefaultJSONCoder().decode(WalletApiModel.self, from: data)
         } throws: { error in
             guard case .invalidData(let message) = error as? CrossmintServiceError else { return false }
-            return message.contains("\"totp\"")
+            return message.contains("totp")
         }
     }
 }
