db3ae77b3068b0382164c87f5c919073c052d5dd
tomas-martins-crossmint <tomas.temp@paella.dev>
2026-09-25 10:30:54 -0300

    Revert the shared JSON decoder change to keep this branch scoped to wallet config decoding

--- Sources/CrossmintService/DefaultJSONCoder.swift
+++ Sources/CrossmintService/DefaultJSONCoder.swift
@@ -63,11 +63,6 @@ public struct DefaultJSONCoder: JSONCoder {
     ) throws(CrossmintServiceError) -> T where T: Decodable {
         do {
             return try decoder.decode(T.self, from: data)
-        } catch DecodingError.dataCorrupted(let context) {
-            guard let underlyingError = context.underlyingError else {
-                throw .invalidData(context.debugDescription)
-            }
-            throw .invalidData("\(context.debugDescription) (\(underlyingError.localizedDescription))")
         } catch {
             throw .invalidData("\(error.localizedDescription)(\(error)")
         }
